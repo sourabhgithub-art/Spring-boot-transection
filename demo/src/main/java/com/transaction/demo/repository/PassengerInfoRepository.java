0a55bda1e5adb1fc17f63358b525f9dbde19ada5
sourabhgithub-art <choureysourabh06@gmail.com>
2024-06-26 14:27:22 +0530

    Changes in transaction folder structure

--- demo/src/main/java/com/transaction/demo/repository/PassengerInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.transaction.demo.repository;
-
-import com.transaction.demo.model.PassengerInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassengerInfoRepository extends JpaRepository<PassengerInfo, Long> {
-}
---
+++ src/main/java/com/transaction/demo/repository/PaymentInfoRepository.java
@@ -0,0 +1,9 @@
+package com.transaction.demo.repository;
+
+import com.transaction.demo.model.PaymentInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentInfoRepository extends JpaRepository<PaymentInfo, String> {
+}
